45878fedd99d21d44f9f0d7a4188be60331f5f9e
Sevinch-najmiddinova <najmiddinova2021@mail.ru>
2021-11-05 10:33:18 +0500

    save changes holidaycard commit

---
+++ src/Components/HolidayCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HolidayCard = () => {
+    return (
+        <div>
+            <h2>this is HolidayCard</h2>
+        </div>
+    )
+}
+
+export default HolidayCard
