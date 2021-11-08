f8187c89dab0cd5f553a817e36c74874ca46f40c
sabohat-ambassador <86459708+sabohat-ambassador@users.noreply.github.com>
2021-11-08 10:06:39 +0500

    header changed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import  '../Header/header.css'
 const Header = () => {
   return (
-    <div className="header">
+    <div className="header" style={{ backgroundImage: 'url(/img/People-fireworks.jpg)'}}>
+      <div className='container'>
+
       <div className="row">
+        <nav className="navbar">
         <Link className="header-link" to="/">
           Home
         </Link>
-        <nav className="navbar">
           <Link to="/favourites" className="navbar__link">
             My Favourites
           </Link>
           <Link className="navbar__link" to="/holidays"> Holiday</Link>
           <Link className="navbar__link" to="/about-us"> About us</Link>
         </nav>
+        <div className='log_in'>
+          <Link to='log-in'>Log in</Link>
+        </div>
+      </div>
       </div>
     </div>
   );
