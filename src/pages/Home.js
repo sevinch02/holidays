a7b4e81e483d0fa75eba96b9055532f2691f42c6
sabohat-ambassador <86459708+sabohat-ambassador@users.noreply.github.com>
2021-11-08 10:55:10 +0500

    header changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,9 @@ import react from "react";
 
 const Home=()=>{
     return (
-        <h1>FIESTA</h1>
+        <section className='first-section'>
+            <h1 className='sayt-name'>FIESTA</h1>
+        </section>
     )
 }
 
