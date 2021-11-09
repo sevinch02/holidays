3ffb0c8645091ca6731726c1820a8fd1e8dfcb3a
sabohat-ambassador <86459708+sabohat-ambassador@users.noreply.github.com>
2021-11-09 09:58:19 +0500

    changed

--- src/Components/HolidaysList/Holiday.js
+++ src/Components/HolidaysList/Holiday.js
@@ -6,6 +6,25 @@ const Holiday=()=>{
         <div container='container'>
             <h1 className='todays_holiday'>Today's holidays</h1>
             <div className='card'>
+            <div class="image">
+  <span><img src="/img/eid.jpg" alt=""/></span>
+<div id="container">
+
+<svg version="1.1"
+	 xmlns="http://www.w3.org/2000/svg"
+	 x="0px" y="0px" viewBox="0 0 3841 108.5"
+	 style={{ overflow: 'scroll', enableBackground:'new 0 0 3841 108.5'}} >
+<defs>
+</defs>
+<path style={{fill: '#FFFFFF', strokeMiterlimit:'10'}} d="M3360.5,97.739c-242,0-480-48.375-480-48.375
+	S2647.5,0.5,2400.5,0.5s-480,48.375-480,48.375s-238,48.864-480,48.864s-480-48.375-480-48.375S727.5,0.5,480.5,0.5
+	S0.5,48.875,0.5,48.875V108h1920h1920V48.875C3840.5,48.875,3602.5,97.739,3360.5,97.739z"/>
+</svg>
+
+</div>
+</div>
+<div class="white">
+</div>
                 <img src='/img/eid.jpg'/>
                 <div className='cards_text'>
 
