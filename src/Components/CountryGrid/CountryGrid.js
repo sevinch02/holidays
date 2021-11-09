9a34486453ef0fd59834d473532208a689db1d54
Sevinch-najmiddinova <najmiddinova2021@mail.ru>
2021-11-09 11:24:01 +0500

    grid commit

--- src/Components/CountryGrid/CountryGrid.js
+++ src/Components/CountryGrid/CountryGrid.js
@@ -8,35 +8,75 @@ import { Row, Col, Divider } from 'antd';
 const CountryGrid = () => {
     const DemoBox = props => <p className={`height-${props.value}`}>{props.children}</p>;
   
-    return (<div>
-          <Divider orientation="left">Align Middle</Divider>
-    <Row justify="space-around" align="middle">
+    return (
+    <div className="container12">
+    {/* <div style={{backgroundImage : 'url(/img/largge-bg.jpg)', backgroundRepeat : 'no-repeat', backgroundPosition: 'center'  }}  > */}
+    <div ClassName="grid-container" >
+          <img ClassName="grid-item" style={{width: 188 , height: 300}} src="img/dubai.jpg" alt=""/> 
+      <img ClassName="grid-item" style={{width :436 , height: 300}} src="img/london.jpg"alt=""/> 
+         <img  ClassName="grid-item" style={{width :268 ,height: 300}} src="img/korea.jpg"alt="foture"/> 
+         <img  ClassName="grid-item" style={{ width: 468 , height: 300 }} src="img/milano-middle.jpg"alt="foture"/> 
+         <img  ClassName="grid-item" style={{width:241, height:300}} src="img/dubai.jpg"alt="foture"/> 
+        <img  ClassName="grid-item" style={{width: 427 , height: 300 }} src="img/moskva-large.jpg"alt="foture"/> 
+         <img  ClassName="grid-item" style={{width: 481 , height: 300 }} src="img/singopur-large.jpg"alt="foture"/> 
+      <img  ClassName="grid-item" style={{width:214, height:300}} src="img/parij.jpg"alt="foture"/>
+       {/* <img  ClassName="foto-grid-item" style={{width:241, height:300}} src="img/dubai.jpg"alt="foture"/>  */}
+        <img  ClassName="grid-item" style={{width:468 , height:300}}  src="img/rim.jpg"alt="foture"/> 
+        <img ClassName="grid-item" style={{width: 437, height:300}}  src="img/new-york-small.jpg"alt="foture"/> 
+     <img  ClassName="grid-item" style={{width: 492, height:300}}  src="img/san-fransisko.jpg"alt="foture"/> 
+      <img  ClassName="grid-item" style={{width:346 , height:300}}  src="img/ispaniya.jpg"alt="foture"/> 
+    </div>
+ 
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+          {/* <Divider orientation="left">Align Middle</Divider> */}
+    {/* <Row justify="space-around" align="middle">
       <Col span={4}>
-        <DemoBox value={100}>col-4</DemoBox>
+        <DemoBox value={100}>   <img className=" country-grid__img" src="img/istanbul.jpg" alt=""  /> </DemoBox>
       </Col>
       <Col span={4}>
-        <DemoBox value={50}>col-4</DemoBox>
+        <DemoBox value={50}>   <img  className=" country-grid__img" src="img/rim.jpg" alt=""  /> </DemoBox>
       </Col>
       <Col span={4}>
-        <DemoBox value={120}>col-4</DemoBox>
+        <DemoBox value={120}>   <img className="country-grid__img" src="img/london.jpg" alt=""  /> </DemoBox>
       </Col>
       <Col span={4}>
-        <DemoBox value={80}>col-4</DemoBox>
+        <DemoBox value={80}><img className="country-grid__img" src="img/dubai.jpg"  alt="duabi"/></DemoBox>
       </Col>
     </Row>
+
+
+    {/* <Divider orientation="left">Align Bottom</Divider> */}
+    {/* <Row justify="space-between" align="bottom">
+      <Col span={4}>
+        <DemoBox value={100}><img className=" country-grid__img" src="img/moskva-large.jpg"  alt=""/></DemoBox>
+      </Col>
+      <Col span={4}>
+        <DemoBox value={50}><img className=" country-grid__img" src="img/milano-middle.jpg"  alt="roma"/></DemoBox>
+      </Col>
+      <Col span={4}>
+        <DemoBox value={120}> <img className="country-grid__img" src="img/korea.jpg" alt="korea"  />  </DemoBox>
+      </Col>
+      <Col span={4}>
+        <DemoBox value={80}><img className=" country-grid__img" src="img/singopur-large.jpg" alt="singapore"/></DemoBox>
+      </Col>
+    </Row> */} 
     </div>
+    // </div>
     
-        // <div className="CountryGrid">
-        //     <h3>Find by Country</h3>
-        //     <div className="grid-container" >        
-        //     <img className="grid-item country-grid__img" src="img/dubai.jpg"  alt="duabi" /> 
-        //     <img style={{height: 310 , objectFit: 'cover' }} className="grid-item country-grid__img" src="img/istanbul.jpg" alt=""  /> 
-        //     <img className="grid-item country-grid__img " src="img/london.jpg" alt="london "  /> 
-        //     <img className="grid-item country-grid__img" src="img/korea.jpg" alt="korea"  /> 
-        //     <img className="grid-item country-grid__img" src="img/parij.jpg" alt="parij"  />
-        //     <img className="grid-item country-grid__img" src="img/rim.jpg" alt="roma"  /> 
-        //    </div>
-        // </div>
+    
     );
 };
 export default CountryGrid;
