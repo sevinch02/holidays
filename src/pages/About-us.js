51bbc23ef145524c5f514e83bd0129c0c09edde6
sabohat-ambassador <86459708+sabohat-ambassador@users.noreply.github.com>
2021-11-11 13:14:17 +0500

    changed

--- src/pages/About-us.js
+++
@@ -1,9 +0,0 @@
-
-
-const Aboutus=()=>{
-    return (
-        <h3>this is about us page</h3>
-    )
-}
-
-export default Aboutus
--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -3,7 +3,6 @@ import CountriesCatalog from '../Components/CountriesCatalog/CountriesCatalog'
 const CountryCatalog = () => {
     return (
         <div className='countrycards'>
-
             <CountriesCatalog/>
         </div>
     )
