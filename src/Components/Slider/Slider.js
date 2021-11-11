9a170c9f222fcf06464ec36b611a2b8ac0cc5b3d
sabohat-ambassador <86459708+sabohat-ambassador@users.noreply.github.com>
2021-11-11 13:18:34 +0500

    changed

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -13,7 +13,7 @@ const Slider=() => {
   }}
   >
   <SwiperSlide>
-    <div className='bgimage' style={{ backgroundImage: url(/img/eid.jpg)}}>
+    <div className='bgimage' style={{ backgroundImage: `url(/img/eid.jpg)`}}>
       <div className='objects'>
       <h1 className='holiday_name'>Eid</h1>
             <p className='overview'>Partaking in the annual Holi Festival is the most fun I had as a kid. Holi is a Hindu festival of colors, celebrated once a year in March. And growing up in a Hindu household, I absolutely loved the spectacle that came along with celebrating Holi.</p>
@@ -22,7 +22,7 @@ const Slider=() => {
     </div>  
     </SwiperSlide>
     <SwiperSlide>
-    <div className='bgimage' style={{ backgroundImage: url(/img/archa.jpg)}}>
+    <div className='bgimage' style={{ backgroundImage: `url(/img/archa.jpg)`}}>
       <div className='objects'>
       <h1 className='holiday_name'>Eid</h1>
             <p className='overview'>Partaking in the annual Holi Festival is the most fun I had as a kid. Holi is a Hindu festival of colors, celebrated once a year in March. And growing up in a Hindu household, I absolutely loved the spectacle that came along with celebrating Holi.</p>
@@ -31,7 +31,7 @@ const Slider=() => {
     </div>  
     </SwiperSlide>
     <SwiperSlide>
-    <div className='bgimage' style={{ backgroundImage: url(/img/holii.jpg)}}>
+    <div className='bgimage' style={{ backgroundImage: `url(/img/holii.jpg)`}}>
       <div className='objects'>
       <h1 className='holiday_name'>Eid</h1>
             <p className='overview'>Partaking in the annual Holi Festival is the most fun I had as a kid. Holi is a Hindu festival of colors, celebrated once a year in March. And growing up in a Hindu household, I absolutely loved the spectacle that came along with celebrating Holi.</p>
@@ -40,7 +40,7 @@ const Slider=() => {
     </div>  
     </SwiperSlide>
     <SwiperSlide>
-    <div className='bgimage' style={{ backgroundImage: url(/img/christmas.jpg)}}>
+    <div className='bgimage' style={{ backgroundImage: `url(/img/christmas.jpg)`}}>
       <div className='objects'>
       <h1 className='holiday_name'>Eid</h1>
             <p className='overview'>Partaking in the annual Holi Festival is the most fun I had as a kid. Holi is a Hindu festival of colors, celebrated once a year in March. And growing up in a Hindu household, I absolutely loved the spectacle that came along with celebrating Holi.</p>
@@ -57,3 +57,4 @@ const Slider=() => {
   );
 };
 
+export default Slider
