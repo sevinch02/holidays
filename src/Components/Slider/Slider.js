ae0b02bf0833632486e7626c490bfdfc5b5f7cd0
sabohat-ambassador <86459708+sabohat-ambassador@users.noreply.github.com>
2021-11-11 13:16:31 +0500

    changed

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,16 +1,55 @@
-
 import '../Slider/slider.css'
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
 
 const Slider=() => {
+  SwiperCore.use([Autoplay])
   return (
-    <div className='special_holidays' style={{backgroundImage:'url(/img/holii.jpg)'}}>
-
-<div className='about_holiday'>
-
-            <h1 className='holiday_name'>Holi</h1>
+    <Swiper modules={[Autoplay]} spaceBetween={0} slidesPerView={1}
+    autoplay={{
+      delay: 2000, 
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true
+  }}
+  >
+  <SwiperSlide>
+    <div className='bgimage' style={{ backgroundImage: url(/img/eid.jpg)}}>
+      <div className='objects'>
+      <h1 className='holiday_name'>Eid</h1>
             <p className='overview'>Partaking in the annual Holi Festival is the most fun I had as a kid. Holi is a Hindu festival of colors, celebrated once a year in March. And growing up in a Hindu household, I absolutely loved the spectacle that came along with celebrating Holi.</p>
-</div>
+         
         </div>
+    </div>  
+    </SwiperSlide>
+    <SwiperSlide>
+    <div className='bgimage' style={{ backgroundImage: url(/img/archa.jpg)}}>
+      <div className='objects'>
+      <h1 className='holiday_name'>Eid</h1>
+            <p className='overview'>Partaking in the annual Holi Festival is the most fun I had as a kid. Holi is a Hindu festival of colors, celebrated once a year in March. And growing up in a Hindu household, I absolutely loved the spectacle that came along with celebrating Holi.</p>
+         
+        </div>
+    </div>  
+    </SwiperSlide>
+    <SwiperSlide>
+    <div className='bgimage' style={{ backgroundImage: url(/img/holii.jpg)}}>
+      <div className='objects'>
+      <h1 className='holiday_name'>Eid</h1>
+            <p className='overview'>Partaking in the annual Holi Festival is the most fun I had as a kid. Holi is a Hindu festival of colors, celebrated once a year in March. And growing up in a Hindu household, I absolutely loved the spectacle that came along with celebrating Holi.</p>
+         
+        </div>
+    </div>  
+    </SwiperSlide>
+    <SwiperSlide>
+    <div className='bgimage' style={{ backgroundImage: url(/img/christmas.jpg)}}>
+      <div className='objects'>
+      <h1 className='holiday_name'>Eid</h1>
+            <p className='overview'>Partaking in the annual Holi Festival is the most fun I had as a kid. Holi is a Hindu festival of colors, celebrated once a year in March. And growing up in a Hindu household, I absolutely loved the spectacle that came along with celebrating Holi.</p>
+         
+        </div>
+    </div>  
+    </SwiperSlide>
+  </Swiper>
+    
       
 
      
@@ -18,5 +57,3 @@ const Slider=() => {
   );
 };
 
-
-export default Slider
