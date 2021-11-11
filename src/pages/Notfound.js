7b4fff42ead38d80422033350ea489e49a59c208
Abror Abdukadirov <86467446+zumradshukurullayeva@users.noreply.github.com>
2021-11-11 13:03:40 +0500

    ketti

--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -3,7 +3,7 @@
 const NotFound = () => {
     return (
             <div className="container">
-                <h1>404 Error Page</h1>
+                <h1 className='error-text'>404 Error Page</h1>
                 <section class="error-container">
                     <span class="four"><span class="screen-reader-text">4</span></span>
                     <span class="zero"><span class="screen-reader-text">0</span></span>
