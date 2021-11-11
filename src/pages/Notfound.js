d440011670ad431ee1a03a29faf78df5bb5aa551
Abror Abdukadirov <86467446+zumradshukurullayeva@users.noreply.github.com>
2021-11-11 12:57:45 +0500

    endi ko'ringchi

--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -3,11 +3,16 @@
 const NotFound = () => {
     return (
             <div className="container">
+                <h1>404 Error Page</h1>
                 <section class="error-container">
                     <span class="four"><span class="screen-reader-text">4</span></span>
                     <span class="zero"><span class="screen-reader-text">0</span></span>
                     <span class="four"><span class="screen-reader-text">4</span></span>
                 </section>
+
+                <div class="link-container">
+                    <a target="_blank" href="/" class="more-link">Visit the original article</a>
+                </div>
             </div>
     )
 };
