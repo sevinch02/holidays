249d25e101850d5523adeaed225d8de5d39cf5e2
Abror Abdukadirov <86467446+zumradshukurullayeva@users.noreply.github.com>
2021-11-12 09:18:54 +0500

    ketti

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -3,9 +3,18 @@ import './Loader.css'
 
 const Loader = () => {
     return (
-        <div class="loader-container">
-            <div class="loader-icon"></div>
+        <div>
+            <div id="ball">
+                <div id="ball1" className="balls"></div>
+                <div id="ball2" className="balls"></div>
+                <div id="ball3" className="balls"></div>
+            </div>
+
+            <div id="credits">
+                <a href="https://www.buymeacoffee.com/dormin" target="_blank"></a>
+            </div>
         </div>
+    
 
     )
 }
